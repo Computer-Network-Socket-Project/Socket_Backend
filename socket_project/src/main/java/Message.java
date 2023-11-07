250a7b3939e17470d4b5b58065995cf4c65a9f60
Inwoo <lys91892@gmail.com>
2023-11-07 22:21:32 +0900

    new python server commit

--- socket_project/src/main/java/Message.java
+++
@@ -1,9 +0,0 @@
-public class Message {
-    private String content;
-    private String sender;
-
-    public Message(String content, String sender) {
-        this.content = content;
-        this.sender = sender;
-    }
-}
