edb0ac802977f3f7f7485e8d88e4445af2b97f14
Wono <129478826+dnjsgh1204@users.noreply.github.com>
2023-11-08 00:43:23 +0900

    broadcasting client.py
    문자 중계 하는 부분에 HOST IP가 달라질 때마다 하는 법을 찾는 중입니다. 그 이외 변경사항 없습니다.

--- client.py
+++ client.py
@@ -1,47 +1,31 @@
 import socket
-import threading
+from _thread import *
 
-# 클라이언트 소켓을 생성합니다.
+HOST = ''  # 서버의 IP 주소 입력 연결 마다 HOST 값 맞게 작성하는 법 미완성.
+PORT = 9999
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
 
-# 서버의 IP 주소와 포트 번호를 설정합니다.
-SERVER_HOST = '0.0.0.0'
-SERVER_PORT = 12345
+# 클라이언트 유형을 입력 받음
+client_type = input("Enter client type ('relay' or 'viewer'): ")
 
-# 클라이언트의 ID를 설정합니다.
-client_id = "creator"  # 이 값을 "viewer"로 변경하여 볼 수만 있는 클라이언트로 사용 가능
-
-# 서버에 연결합니다.    연결이 잘 안되면 host, port부분 오류일 수 있으니 잘 확인.
-client_socket.connect((SERVER_HOST, SERVER_PORT))
-client_socket.send(client_id.encode())
-
-# 메시지를 보내는 함수
-def send_message():
+def recv_data(client_socket):
     while True:
-        message = input("메시지를 입력하세요: ")
-        client_socket.send(message.encode())
+        data = client_socket.recv(1024)
+        print("Received: " + repr(data.decode()))
 
-# 메시지를 받는 함수
-def receive_message():
-    while True:
-        message = client_socket.recv(1024).decode() #server와 동일하게 값 설정
-        print("수신: " + message)
+start_new_thread(recv_data, (client_socket,))
+print('>> Connect Server')
 
-def exit_chat():
-    message = "exit"
-    client_socket.send(message.encode())
-    client_socket.close()
-
-# 메시지를 보내는 스레드 시작
-send_thread = threading.Thread(target=send_message)
-send_thread.daemon = True
-send_thread.start()
-
-# 메시지를 받는 스레드 시작
-receive_thread = threading.Thread(target=receive_message)
-receive_thread.daemon = True
-receive_thread.start()
-
-# 클라이언트를 계속 실행
 while True:
-    pass
+    message = input()
+
+    if message == 'quit':
+        break
+
+    # 클라이언트 유형과 메시지를 서버로 전달
+    message_with_type = f"{client_type}:{message}"
+    client_socket.send(message_with_type.encode())
+
+client_socket.close()
