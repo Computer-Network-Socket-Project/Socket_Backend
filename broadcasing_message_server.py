77cecf053170af4fdcdfcacb355d4df0231ddb03
Wono <129478826+dnjsgh1204@users.noreply.github.com>
2023-11-10 15:48:01 +0900

    Update broadcasing_message_server.py
    clinet_types setting

--- broadcasing_message_server.py
+++ broadcasing_message_server.py
@@ -4,11 +4,13 @@ import socket
 from _thread import *
 
 client_sockets = []
+client_types = {}  # 각 클라이언트의 타입을 저장하는 딕셔너리 추가
 
 ## Server IP and Port ##
 
-HOST = socket.gethostbyname(socket.gethostname())
+HOST = '0.0.0.0'  # 모든 사용 가능한 네트워크 인터페이스를 의미
 PORT = 9999
+
 
 ########## processing in thread ##
 ## new client, new thread ##
@@ -26,15 +28,14 @@ def threaded(client_socket, addr):
                 print('>> Disconnected by ' + addr[0], ':', addr[1])
                 break
 
-            client_types = {True: 1, False: 0}  # 1 :방 생성자 , 0: 경기 관람
             message = data.decode().split(':', 1)[1]  # ':'로 구분된 첫 번째 ':' 이후의 내용은 메시지
 
-            if client_types[client_type]:
+            if client_types[client_socket]:  # 딕셔너리에서 클라이언트 소켓을 키로 사용
                 # 중계 메시지를 입력하는 클라이언트의 경우 다른 중계 클라이언트에게 메시지 전달
                 for client in client_sockets:
                     if client != client_socket and 'relay' in client.recv(1024).decode():
                         client.send(message.encode())
-            elif not client_types[client_type]:
+            else:
                 # 그냥 메시지를 받아보는 클라이언트의 경우 메시지 출력
                 print('>> Received from ' + addr[0], ':', addr[1], message)
 
@@ -54,6 +55,7 @@ def threaded(client_socket, addr):
 
     client_socket.close()
 
+
 ############# Create Socket and Bind ##
 
 print('>> Server Start with ip :', HOST)
@@ -70,8 +72,10 @@ try:
 
         client_socket, addr = server_socket.accept()
         client_sockets.append(client_socket)
+        client_type = bool(int(input("Enter client type (1 for 방 생성자, 0 for 경기 관람): ")))  # 클라이언트 타입 입력 받기
+        client_types[client_socket] = client_type  # 딕셔너리에 클라이언트 소켓과 타입 저장
         start_new_thread(threaded, (client_socket, addr))
-        print("참가자 수 : ", len(client_sockets)) #클라이언트 수 확
+        print("참가자 수 : ", len(client_sockets))  # 클라이언트 수 확
 except Exception as e:
     print('에러 : ', e)
 
