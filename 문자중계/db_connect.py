b3e2d0825fd6135215301b2bddc54647763bef45
Wono <129478826+dnjsgh1204@users.noreply.github.com>
2023-11-12 01:34:39 +0900

    Delete 문자중계/db_connect.py

--- 문자중계/db_connect.py
+++
@@ -1,8 +0,0 @@
-import pymysql
-
-db = pymysql.connect(host='localhost',
-                     user='root',
-                     password='Odmc7721!!',
-                     db='project_cn')
-
-cursor = db.cursor()
